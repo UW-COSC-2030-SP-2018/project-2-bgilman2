1cf8398428d345a23b54f3cd78acd188d864e4f3
Brett GIlman <31521666+bgilman2@users.noreply.github.com>
2018-04-22 16:43:59 -0600

    Initial commit

---
+++ Functions.h
@@ -0,0 +1,52 @@
+// By Brett Gilman and Jordan Ruckle
+// 4/22/2018
+// Functions.h
+
+#include<vector>
+#include<string>
+#include<map>
+
+//from string
+using std::string;
+
+//from vector
+using std::vector;
+
+//from map
+using std::map;
+using std::pair;
+
+//The purpose of this function is to recursively sort
+//a passed in vector of integers using the quick sort method
+void QuickSort(vector<int> Vec)
+{
+	return; //terminate the function
+}
+
+//The purpose of this function is to recursively find an integer
+//that is passed in to the function using the Binary Search Method
+int BinarySearch(vector<int> Vec, int X)
+{
+	return -1; //return that the element was not found
+}
+
+//The purpose of this function is to recursively sort
+//a passed in vector of integers using the merge sort method
+void MergeSort(vector<int> Vec)
+{
+	return; //terminate the function
+}
+
+//The purpose of this function is to use a custom hashing function to convert
+//a passed in string into a hash
+int HashFunction(string Input)
+{
+	int Hash = 0;
+	return Hash; //return the hashed string
+}
+
+
+void BloomFilter()
+{
+	return 0;
+}
---
+++ TestBed.cpp
@@ -0,0 +1,22 @@
+// By Brett Gilman and Jordan Ruckle
+// 4/22/2018
+// TestBed.cpp
+
+/////////////// PURPOSE ///////////////
+// The purpose of this program is to //
+// test the five functions that will //
+// be implemented in Functions.h     //
+///////////////////////////////////////
+
+//importing functions
+#include<iostream>
+
+//from iostream
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	return 0; //terminat the function
+}
