eb7efa7203aabd31f51adffe395ebc3fa844c54d
Brett GIlman <31521666+bgilman2@users.noreply.github.com>
2018-04-25 17:30:41 -0600

    Added sort checking function

--- TestBed.cpp
+++ TestBed.cpp
@@ -11,26 +11,40 @@
 //importing functions
 #include<iostream>
 #include"Functions.h"
+#include<algorithm>
 
 //from iostream
 using std::cout;
 using std::cin;
 using std::endl;
 
+//from algorithm
+using std::sort;
+
 //function for printing vectors
 void PrintVector(vector<int> Vec, string VecName);
+//function for testing if the custom sort function was successful
+void CheckSort(vector<int> VecSTL, vector<int> VecCustom);
 
 int main()
 {
 	///////////////// TESTING QUICKSORT /////////////////
 	vector<int> QuickSortVec = {8,9,6,5,7,8,3,2,1,0,10,4}; //vector for testing quicksort function
-	//vector<int> QuickSortVec = { 8,9,6,5,7 };
+	/* Manual Checking:
 	//print the unsorted vector
 	PrintVector(QuickSortVec, "Quick Sort Vector (unsorted)");
 	//sort the vector
 	QuickSort(QuickSortVec, 0 ,QuickSortVec.size() - 1);
 	//print the sorted vector
 	PrintVector(QuickSortVec, "Quick Sort Vector (sorted)");
+	*/
+
+	/*Auto Checking:*/
+	cout << "Testing QuickSort Function: ";
+	vector<int> QuickSTLVec = { 8,9,6,5,7,8,3,2,1,0,10,4 }; //vector for testing quicksort function
+	QuickSort(QuickSortVec, 0, QuickSortVec.size() - 1); //sort the vector
+	CheckSort(QuickSTLVec, QuickSortVec); //check if the STL sorted vector and the custom sort vector are the same
+
 	/////////////////////////////////////////////////////
 
 	///////////////// TESTING BINARY SEARCH /////////////////
@@ -79,4 +93,34 @@ void PrintVector(vector<int> Vec, string VecName)
 	for (int i = 0; i < Vec.size(); i++)
 		cout << Vec[i] << endl;
 	cout << "------------------------------" << endl;
+}
+
+//The purpose of this function is to test if two vectors are the same. The VecSTL is sorted before the comparison
+//happens and VecCustom is assumed to already be sorted
+void CheckSort(vector<int> VecSTL, vector<int> VecCustom)
+{
+	//assuming VecCustom is already sorted
+	//sort VecSTL using the STL sort
+	sort(VecSTL.begin(), VecSTL.end());
+	//if the vectors are the same size
+	if (VecSTL.size() == VecCustom.size())
+	{
+		//iterate throught the vectors and check if they are equal to each other
+		for (int i = 0; i < VecSTL.size() - 1; i++)
+		{
+			//if one element is not the same
+			if (VecSTL[i] != VecCustom[i])
+			{
+				cout << "Failed" << endl;
+				return; //return that they are not the same
+			}
+		}
+		cout << "Passed" << endl;
+		return; //if program gets here, the two vectors were the same
+	}
+	else
+	{
+		cout << "Failed" << endl;
+		return; //vectors are not the same size so they are not equal
+	}
 }
