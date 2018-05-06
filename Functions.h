7690ab7ed3b995748c319e9c4585fe76032f268b
jdruckle <35939096+jdruckle@users.noreply.github.com>
2018-05-06 13:56:23 -0600

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -9,25 +9,7 @@ using std::string;
 
 // defining a bloom filter structure and using the hash function
 // type define from Lab 10
-typedef unsigned int(*hash_function)(const void *data);
-typedef struct bloomFilter * bloom;
-
-// Structure of functions to be used by Bloom Filters. Contains a hash function
-// and a pointer to the next hash function. Is a linked list!
-struct bloomFilterFunction 
-{
-	hash_function hashFunction;
-	struct bloomFilterFunction *next;
-};
-
-// Structure of the Bloom Filter itself. Contains the list of hash functions from the
-// previous struct, a set of bits, and a set size.
-struct bloomFilter 
-{
-	struct bloomFilterFunction *hashFunction;
-	int *bits;
-	size_t bloomSize;
-};
+typedef unsigned int(*hash_function)(const std::string&);
 
 //function declarations
 void QuickSort(int arr[], int StartIndex, int EndIndex);
@@ -35,11 +17,7 @@ int Partition(int arr[], int StartIndex, int EndIndex);
 int BinarySearch(int arr[], int X, int StartIndex, int EndIndex);
 void MergeSort(int arr[], int size);
 void Merge(int arr[], int Left[], int LeftSize, int Right[], int RightSize);
-int HashFunction(string Input);
-bloom createBloom(size_t size);
-void addHashFunctionToBloom(bloom bFilter, hash_function function);
-void addToBloom(bloom bFilter, void *element);
-bool isPresent(bloom bFilter, void *element);
+unsigned int OurHash(const string& Input);
 
 //The purpose of this function is to recursively sort
 //a passed in vector of integers using the quick sort method
@@ -98,23 +76,23 @@ int Partition(int arr[], int StartIndex, int EndIndex)
 //that is passed in to the function using the Binary Search Method
 int BinarySearch(int arr[], int X, int StartIndex, int EndIndex)
 {
-	int MiddleIndex = (StartIndex + EndIndex)/2; //get the middle index
-	//check if x is equal to the start index
+	int MiddleIndex = (StartIndex + EndIndex) / 2; //get the middle index
+												   //check if x is equal to the start index
 	if (X == arr[StartIndex])
 		return StartIndex;
 	//check if X is equal to the middle index
 	else if (X == arr[MiddleIndex])
 		return MiddleIndex; //return that X was found
-	//check if x is equal to the end index
+							//check if x is equal to the end index
 	else if (X == arr[EndIndex])
 		return EndIndex;
 	//if there are no more elements left in the list, X was not found
 	else if ((MiddleIndex == StartIndex) || (MiddleIndex == EndIndex))
 		return -1; //return that the element was not found
-	//check if X is less than the middle index
+				   //check if X is less than the middle index
 	else if (X < arr[MiddleIndex])
 		return BinarySearch(arr, X, StartIndex, MiddleIndex); //check the left half of the vector
-	//check if X is greater than the middle index
+															  //check if X is greater than the middle index
 	else if (X > arr[MiddleIndex])
 		return BinarySearch(arr, X, MiddleIndex, EndIndex); //check the right half of the vector
 }
@@ -128,7 +106,7 @@ void MergeSort(int arr[], int size)
 	//no code was copied and only the algorithm for Merge Sort was used, not the function Merge (Merge function is original)
 
 	int n = size; //get the length of the array
-	//if there is only one element left, exit the function
+				  //if there is only one element left, exit the function
 	if (n < 2)
 		return;
 	int Middle = n / 2; //get the middle index of the array
@@ -137,18 +115,18 @@ void MergeSort(int arr[], int size)
 	int *Left = new int[LeftSize]; //create a left array of size Middle
 	int *Right = new int[RightSize]; //create a right array of size n - middle
 
-	//fill the left array with the left side of the array
+									 //fill the left array with the left side of the array
 	for (int i = 0; i <= Middle - 1; i++)
 		Left[i] = arr[i];
 
 	//fill the right array with the rioght side of the array
 	for (int i = Middle; i <= n - 1; i++)
 		Right[i - Middle] = arr[i];
-	
+
 	//split the left side of the array
 	MergeSort(Left, Middle);
 	//split the right side of the array
-	MergeSort(Right, n- Middle);
+	MergeSort(Right, n - Middle);
 	//merge the left and right side arrays together
 	Merge(arr, Left, LeftSize, Right, RightSize);
 
@@ -163,25 +141,25 @@ void MergeSort(int arr[], int size)
 void Merge(int arr[], int Left[], int LeftSize, int Right[], int RightSize)
 {
 	//iterator for navigating the unsorted array, left array, and right array, respectively
-	int AIter = 0, 
+	int AIter = 0,
 		LIter = 0,
 		RIter = 0;
 
 	int LVal = 0; //value from the left array to be looked at
 	int RVal = 0; //value from the right array to be looked at
 
-	//while none of the iterators have reached the end of their array
+				  //while none of the iterators have reached the end of their array
 	while ((LIter < LeftSize) || (RIter < RightSize))
 	{
 		//if the left iterator hasn't made it to the end of the array yet
 		if (LIter < LeftSize)
 			LVal = Left[LIter]; //get the next value from the array
 
-		//if the right iterator hasn't made it to the end of the array yet
+								//if the right iterator hasn't made it to the end of the array yet
 		if (RIter < RightSize)
 			RVal = Right[RIter]; //get the next value from the array
 
-		//if the left array is empty
+								 //if the left array is empty
 		if (LIter >= LeftSize)
 		{
 			//save the right value to the unsorted array 
@@ -220,15 +198,16 @@ void Merge(int arr[], int Left[], int LeftSize, int Right[], int RightSize)
 	return; //terminate the function
 }
 
+
 //The purpose of this function is to use a custom hashing function to convert
 //a passed in string into a hash
 // After much trial and error with good prime numbers for collisions and hashing, I was using 57 and 93 as my values. I then did a google search for the
 // "best" prime number for hashing. This in turn led me to the left-shift method which appeared to be much faster and efficient.
-int HashFunction(string Input)
+unsigned int OurHash(const string& Input)
 {
 
 	// Initiate the hash to a number I chose randomly and initiate the intVal integer.
-	unsigned long Hash(8173);
+	unsigned int Hash(8173);
 	int intVal;
 
 	// Interates through the string and creates a hash. The value of the current character of the string is stored in intVal.
@@ -244,22 +223,50 @@ int HashFunction(string Input)
 
 }
 
-// This function is responsible for the creation of a Bloom Filter.
-bloom createBloom(size_t size) {
 
-	bloom theBloom;						// Initialize a Bloom Filter
+// Structure of functions to be used by Bloom Filters. Contains a hash function
+// and a pointer to the next hash function. Is a linked list!
+struct bloomFilterFunction {
+
+	hash_function hashFunction;
+	struct bloomFilterFunction *next;
+
+};
+
+// Structure of the Bloom Filter itself. Contains the list of hash functions from the
+// previous struct, a set of bits, and a set size.
+struct bloomFilter {
+
+	struct bloomFilterFunction *hashFunction;
+	int *bits;
+	size_t bloomSize;
+
+	bloomFilter() {
+		struct bloomFilterFunction *list = new bloomFilterFunction;
+		bloomSize = 0;			// Set the size of the bloom filter
+		bits = new int[0];		// Allocate memory for the bits
+	}
+
+};
+
+// This function is responsible for the creation of a Bloom Filter.
+bloomFilter createBloom(size_t size) {
+
+	bloomFilter *theBloom = new bloomFilter();						// Initialize a Bloom Filter
 	theBloom->bloomSize = size;			// Set the size of the bloom filter
 	theBloom->bits = new int[size];		// Allocate memory for the bits
 
-	return theBloom;					// Return the Bloom Filter
+	return *theBloom;					// Return the Bloom Filter
 
 }
 
 // This function is responsible for adding a hash function to a Bloom Filter's
 // list of functions to iterate through in the isPresent function.
-void addHashFunctionToBloom(bloom bFilter, hash_function function) {
+void addHashFunctionToBloom(bloomFilter bFilter, hash_function function) {
 
-	struct bloomFilterFunction *last = bFilter->hashFunction;		// Initializes a Bloom Filter Function for a tail
+	bloomFilter* bFilterP = &bFilter;
+
+	struct bloomFilterFunction *last = bFilterP->hashFunction;		// Initializes a Bloom Filter Function for a tail
 
 	struct bloomFilterFunction *list = new bloomFilterFunction;		// Initializes a Bloom Filter Function and its head
 	list->hashFunction = function;
@@ -271,7 +278,7 @@ void addHashFunctionToBloom(bloom bFilter, hash_function function) {
 		last->next = list;
 	}
 	else {															// If a list hasn't been created yet, then this function is the first function
-		bFilter->hashFunction = list;
+		bFilterP->hashFunction = list;
 	}
 
 	return;
@@ -279,18 +286,20 @@ void addHashFunctionToBloom(bloom bFilter, hash_function function) {
 }
 
 // This function is responsible for add an element to a Bloom Filter.
-void addToBloom(bloom bFilter, void *element) {
+void addToBloom(bloomFilter bFilter, string element) {
 
-	struct bloomFilterFunction *list = bFilter->hashFunction;		// Initializes a Bloom Filter Function list
+	bloomFilter* bFilterP = &bFilter;
 
-	int *bits = bFilter->bits;										// Stores the Bloom Filter's bits in a pointer
+	struct bloomFilterFunction *list = bFilterP->hashFunction;		// Initializes a Bloom Filter Function list
+
+	int *bits = bFilterP->bits;										// Stores the Bloom Filter's bits in a pointer
 
 
-																	// Iterates through the list of hashes that has been provided and calculates each hash for the
-																	// supplied element.
-	while (list) {
+	// Iterates through the list of hashes that has been provided and calculates each hash for the
+	// supplied element.
+	while (list) {													
 		unsigned int hash = list->hashFunction(element);			// Computes the hash of the function
-		hash %= bFilter->bloomSize * 8;								// A hash to be used for verification
+		hash %= bFilterP->bloomSize * 8;							// A hash to be used for verification
 		bits[hash / 8] |= 1 << hash % 8;							// Calculates the Bloom Filter's nth bit. The hash is n
 		list = list->next;											// Iterates to the next item
 	}
@@ -300,15 +309,17 @@ void addToBloom(bloom bFilter, void *element) {
 }
 
 
-bool isPresent(bloom bFilter, void *element) {
+bool isPresent(bloomFilter bFilter, string element) {
 
-	struct bloomFilterFunction *list = bFilter->hashFunction;		// Initializes a Bloom Filter Function list
+	bloomFilter* bFilterP = &bFilter;
 
-	int *bits = bFilter->bits;										// Stores the Bloom Filter's bits in a pointer
+	struct bloomFilterFunction *list = bFilterP->hashFunction;		// Initializes a Bloom Filter Function list
+
+	int *bits = bFilterP->bits;										// Stores the Bloom Filter's bits in a pointer
 
 	while (list) {
 		unsigned int hash = list->hashFunction(element);			// Computes the hash of the function
-		hash %= bFilter->bloomSize * 8;								// A hash to be used for verification
+		hash %= bFilterP->bloomSize * 8;								// A hash to be used for verification
 
 		if (!(bits[hash / 8] & 1 << hash % 8)) {					// Checks the nth bit and returns false if it is 0
 			return false;
